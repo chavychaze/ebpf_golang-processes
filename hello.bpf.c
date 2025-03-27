b7bfe45e5326270e55299adce20d5bf94cc1f7a8
chavychaze <vitaliysobol@gmail.com>
2025-03-27 17:52:24 +0200

    Create hello.bpf.c

---
+++ hello.bpf.c
@@ -0,0 +1,12 @@
+// +build ignore
+#include "hello.hbpf.h"
+
+BPF_PEERF_OUTPUT(gotopia);
+SEC("kprobe/sys_execve");
+int hello(void *ctx)
+{
+  char data[30];
+  bpf_get_current_comm(&data, sizeof(data));
+  bpf_perf_event_output(ctx, &gotopia, BPF_F_CURRENT_CPU, &data, sizeof(data));
+  return 0;
+}
